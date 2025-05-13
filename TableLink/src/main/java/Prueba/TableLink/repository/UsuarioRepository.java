5f05f907867e75108e550d89001f2a2ff06b3969
Deybi-hud <hu4nt4r@gmail.com>
2025-05-13 15:29:47 -0400

    Post arreglado

--- TableLink/src/main/java/Prueba/TableLink/repository/UsuarioRepository.java
+++ TableLink/src/main/java/Prueba/TableLink/repository/UsuarioRepository.java
@@ -4,10 +4,6 @@ package Prueba.TableLink.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Prueba.TableLink.model.Usuario;
-import java.util.List;
-
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    List<Usuario> findById(Integer id);
 }
