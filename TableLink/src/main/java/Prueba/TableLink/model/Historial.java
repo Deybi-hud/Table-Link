aaf63c486ba3bd3d0755c72f2ec9b53a669b0254
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-05-13 16:09:21 -0400

    colaboracion

--- TableLink/src/main/java/Prueba/TableLink/model/Historial.java
+++ TableLink/src/main/java/Prueba/TableLink/model/Historial.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,5 +31,9 @@ public class Historial {
 
     @Column(nullable = false)
     private String tipoConversion;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 
 }
