7f2f3f0d3051d77d136aee37f619288d1c699a64
Deybi-hud <hu4nt4r@gmail.com>
2025-05-20 14:11:14 -0400

    Update

--- TableLink/src/main/java/Prueba/TableLink/model/usuario.java
+++ TableLink/src/main/java/Prueba/TableLink/model/usuario.java
@@ -1,5 +1,28 @@
 package Prueba.TableLink.model;
 
-public class Usuario {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Table(name= "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 20, nullable =false)
+    private String nombreUsuario;
+
+    @Column(nullable = false)
+    private String contrasena;
 }
