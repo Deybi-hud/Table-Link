e05941b23b3296fc89143da090a921f8f3a21e4c
Deybi-hud <hu4nt4r@gmail.com>
2025-05-20 17:28:50 -0400

    update

--- TableLink/src/main/java/Prueba/TableLink/repository/UsuarioRepository.java
+++ TableLink/src/main/java/Prueba/TableLink/repository/UsuarioRepository.java
@@ -1,20 +1,12 @@
 package Prueba.TableLink.repository;
 
-
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import Prueba.TableLink.model.Historial;
 import Prueba.TableLink.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByNombreUsuario(String  nombreUsuario);
-
-    @Query("SELECT u FROM  u WHERE u.usuario.id = :usuarioId")
-    List<Historial> obtenerUsuario(@Param("usuarioId") Long usuarioId);
 }
